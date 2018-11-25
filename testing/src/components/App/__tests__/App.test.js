7539e28bcd9c64bc6848b49c124010238ffdc01d
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-11-25 01:52:36 -0200

    Verifying Component Existence

--- testing/src/components/App/__tests__/App.test.js
+++ testing/src/components/App/__tests__/App.test.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from '..';
 
-it('should render without crash', () => {
-  expect(2).toEqual(2);
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Comment Box');
+
+  ReactDOM.unmountComponentAtNode(div);
 });
