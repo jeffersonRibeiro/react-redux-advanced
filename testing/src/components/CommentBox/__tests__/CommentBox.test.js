7802aa4d9c19819d2c486c9e21e72ef266a43884
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 15:38:36 -0200

    Describe Statements

--- testing/src/components/CommentBox/__tests__/CommentBox.test.js
+++ testing/src/components/CommentBox/__tests__/CommentBox.test.js
@@ -18,28 +18,24 @@ it('has a textarea and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: {
-      value: 'new comment'
-    }
+describe('the textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: {
+        value: 'new comment'
+      }
+    });
+
+    wrapped.update();
   });
 
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('should submit the form and clear the textarea', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: {
-      value: 'new comment'
-    }
+  it('has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
   });
 
-  wrapped.update();
-
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+  it('should submit the form and clear the textarea', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
