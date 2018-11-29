a698b964077d7e4ae415aaade1f51dfc0723bab9
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 11:21:44 -0200

    Expecting the Comment List

--- testing/src/components/App/__tests__/App.test.js
+++ testing/src/components/App/__tests__/App.test.js
@@ -3,11 +3,17 @@ import { shallow } from 'enzyme';
 
 import App from '..';
 import CommentBox from '../../CommentBox';
-
+import CommentList from '../../CommentList';
 
 it('shows a comment box', () => {
   const wrapped = shallow(<App />);
 
   expect(wrapped.find(CommentBox).length).toEqual(1);
 
+});
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
+  
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
