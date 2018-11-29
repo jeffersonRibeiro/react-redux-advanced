1d882135ed49cab9e41467f7cf0326e5786af563
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 15:53:21 -0200

    The Provider Tag

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from 'services/reducers';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers, {})}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
