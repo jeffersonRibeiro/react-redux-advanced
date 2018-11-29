6d22afa67f8ef79d1b80bc9989f73d55a1a89995
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 14:30:39 -0200

    Asserting Element Existence

--- testing/src/components/CommentBox/__tests__/CommentBox.test.js
+++ testing/src/components/CommentBox/__tests__/CommentBox.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { mount } from 'enzyme';
 
-import CommentBox from 'component/CommentBox';
+import CommentBox from 'components/CommentBox';
 
 it('has a textarea and a button', () => {
+  const wrapped = mount(<CommentBox />);
 
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
 });
