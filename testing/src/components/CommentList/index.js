fafb04eef2dc048cf6a37e021432da8de30fcc37
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 17:54:04 -0200

    Comment List Wireup

--- testing/src/components/CommentList/index.js
+++ testing/src/components/CommentList/index.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-  return <div>Comment List</div>;
-};
+class CommentList extends Component {
+
+  renderComments = () => {
+    const { comments } = this.props;
+    return comments.map(comment => (
+      <li key={comment}>{comment}</li>
+    ));
+  }
+  
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    comments: state.comments
+  }
+}
+
+export default connect(mapStateToProps)(CommentList);
