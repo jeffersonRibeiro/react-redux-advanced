08bd4e67478dd3cc2b7be3caf0a856537c7f3866
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-29 11:55:14 -0200

    Code Reuse with beforeEach

--- testing/src/components/App/__tests__/App.test.js
+++ testing/src/components/App/__tests__/App.test.js
@@ -5,15 +5,17 @@ import App from '..';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
 
 });
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
-  
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
