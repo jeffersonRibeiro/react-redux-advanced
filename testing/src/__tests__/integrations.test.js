55a45e53f938747adb6808b46857722105389cd7
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-30 11:52:43 -0200

    Integration Tests in Action

---
+++ testing/src/__tests__/integrations.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Root from 'Root';
+import App from 'components/App';
+
+it('can fetch a list of comments and display them', () => {
+  /* Attempt to render the *entire* app */
+  /* find the 'fetchComments' button and click it */
+  /* expect to find a list of comments! */
+});
