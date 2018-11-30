93bcfcf2131a6b61a94f38e4acaf3947246d0afd
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-30 11:44:50 -0200

    Parsing Comment list

--- testing/src/components/CommentBox/index.js
+++ testing/src/components/CommentBox/index.js
@@ -22,16 +22,19 @@ class CommentBox extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add a Comment</h4>
-        <textarea
-          value={this.state.comment}
-          onChange={this.handleChange('comment')}
-        />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea
+            value={this.state.comment}
+            onChange={this.handleChange('comment')}
+          />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }
