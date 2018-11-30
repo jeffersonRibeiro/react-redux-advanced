cb4c44a043b39cf3eac6c7be8a5b03e39c6c9ef8
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-30 13:06:07 -0200

    Adding routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Root from 'Root';
-import App from './components/App';
+import Root from "Root";
+import App from "./components/App";
 
 ReactDOM.render(
   <Root>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route render={() => <div>Not Found</div>} />
+      </Switch>
+    </BrowserRouter>
   </Root>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
