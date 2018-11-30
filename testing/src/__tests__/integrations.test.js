dde38d399c8e9ac21b875bc85bae35356018a7dd
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-11-30 12:12:04 -0200

    Faking requests with moxios

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import moxios from 'moxios';
 import { mount } from 'enzyme';
 
 import Root from 'Root';
 import App from 'components/App';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it('can fetch a list of comments and display them', () => {
   /* Attempt to render the *entire* app */
