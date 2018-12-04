97b21373313cb72433ecde6041cb477291344751
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-12-04 16:23:07 -0200

    Forced Navigation with React Router

--- src/components/CommentBox/index.js
+++ src/components/CommentBox/index.js
@@ -18,7 +18,7 @@ class CommentBox extends Component {
 
   shouldNavigateAway() {
     if (!this.props.auth) {
-      console.log('I NEED TO LEAVE!!!');
+      this.props.history.push('/');
     } else {
       console.log(':)');
     }
