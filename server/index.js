eb5adcbe5b2f6318e1a4a080423e74a60bb9ea34
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-12-05 17:46:39 -0200

    More Server setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+/* App setup */
+
+/* Server setup */
+const port = process.env.PORT || 3090;
+app.listen(port, () => console.log('Server listening on:', port));
