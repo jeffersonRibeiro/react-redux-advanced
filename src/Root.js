8d29cb1b986a787309ec26240c4511f29f9088e2
tvglobo <jefferson.ribeiro.contato@gmail.com>
2018-12-05 17:18:26 -0200

    State Validation Middleware

--- src/Root.js
+++ src/Root.js
@@ -4,13 +4,10 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 
 import reducers from 'services/reducers';
+import async from 'middlewares/async';
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    applyMiddleware(reduxPromise)
-  );
+  const store = createStore(reducers, initialState, applyMiddleware(async));
 
   return <Provider store={store}>{children}</Provider>;
 };
